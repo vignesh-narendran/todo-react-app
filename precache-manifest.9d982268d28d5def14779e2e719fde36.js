36e62bf77c85d21446c7817994725dedd4e98df1
Vignesh Narendran <vignesh-narendran@users.noreply.github.com>
2020-03-09 20:45:07 +0530

    Updates

---
+++ precache-manifest.9d982268d28d5def14779e2e719fde36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7991882b8dc1c47559051b59fb6b1a",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "b67c5fd1e252a30597a9",
+    "url": "/todo-react-app/static/css/main.934509e1.chunk.css"
+  },
+  {
+    "revision": "33929902543396f19018",
+    "url": "/todo-react-app/static/js/2.5d221190.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todo-react-app/static/js/2.5d221190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67c5fd1e252a30597a9",
+    "url": "/todo-react-app/static/js/main.46ec7d1a.chunk.js"
+  },
+  {
+    "revision": "6aa1b47b0aa51420e1ce",
+    "url": "/todo-react-app/static/js/runtime-main.de9b657e.js"
+  }
+]);
